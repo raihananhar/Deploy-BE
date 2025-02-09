405e14fd83a474b4224c168dac7ea04d8936355d
raihananhar <raihanannr@gmail.com>
2025-02-09 14:47:33 +0700

    eror ges

--- backend/settings.py
+++ backend/settings.py
@@ -9,10 +9,11 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
-from pathlib import Path
-import dj_database_url
 import os
+from pathlib import Path
+import pymysql
+pymysql.install_as_MySQLdb()
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
